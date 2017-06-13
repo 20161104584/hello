a80c1ef82f4930b945b5b6d9f121463ebffd000e
20161104584 <a20161104584@mac26.local>
2017-06-13 11:06:25 +0800

    hello
    hello初始

---
+++ hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello
+//
+//  Created by 20161104584 on 17/6/7.
+//  Copyright © 2017年 20161104584. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
